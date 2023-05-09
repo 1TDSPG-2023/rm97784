6162929104cb550b119b8f0a03e788b9abc5bc57
adrian-siv <adrian.ssiv@gmail.com>
2023-05-08 22:04:34 -0300

    Tentando fazer o item 2 do exercício

--- exercicios/exercicio_1/exercicio-1.js
+++ exercicios/exercicio_1/exercicio-1.js
@@ -1,5 +1,20 @@
-const botao = document.querySelector("#botao_background");
+const botao_background = document.querySelector("#botao_background");
+const botao_enviar = document.querySelector("#enviar_input_texto");
+let inputUsuario
 
-botao.addEventListener("click", ()=>{
+
+botao_background.addEventListener("click", () => {
     document.querySelector("body").setAttribute("style", "background-color:pink");
 })
+
+botao_enviar.addEventListener("click", () => {
+    lista = document.querySelector("body").innerHTML += "<ul id='lista'></ul>"
+    const newItem = document.createElement("li");  
+    // inputUsuario = document.querySelector("#input__usuario").value;
+    // newItem.appendChild(inputUsuario);
+    lista.appendChild(newItem);
+
+    console.log();
+})
+
+
