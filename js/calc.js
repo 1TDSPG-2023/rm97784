3e51e2b7323f12b58cde9ed58d0c61132ed3b4d6
adrian-siv <adrian.ssiv@gmail.com>
2023-05-12 12:26:33 -0300

    finalizada calculadora

--- js/calc.js
+++ js/calc.js
@@ -1,5 +1,11 @@
 function insereValorNoVisor(botao, visor) {
-    visor.value += botao.value
+    if(botao.value === "+" || botao.value === "-" || botao.value === "*" || botao.value === "/"){   
+        if(visor.value[visor.value.length - 1] != "+" && visor.value[visor.value.length - 1] != "-" && visor.value[visor.value.length - 1] != "*" && visor.value[visor.value.length - 1] != "/") {
+            visor.value += botao.value;
+        }
+    } else {
+        visor.value += botao.value;
+    }
 }
 
 const resultadoDaOperacao = (visor) => {
