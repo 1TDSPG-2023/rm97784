dc0d45841e7be300ec078f58316c0052a41dfccb
adrian-siv <adrian.ssiv@gmail.com>
2023-05-17 09:53:45 -0300

    Adicionado armazenamento em localStorage

--- js/login.js
+++ js/login.js
@@ -1,32 +1,50 @@
-//Gerando um token com Math.
-let tokenGerado = Math.random().toString(16).substring(2);
+let listaDeUsuarios = [
+  {
+    nomeCompleto: "Denden da Silva",
+    nomeUsuario: "denden",
+    senhaUsuario: "123456",
+  },
+  {
+    nomeCompleto: "Gersu da Silva",
+    nomeUsuario: "gege",
+    senhaUsuario: "123456",
+  },
+  {
+    nomeCompleto: "José da Silva",
+    nomeUsuario: "jose",
+    senhaUsuario: "123456",
+  },
+  {
+    nomeCompleto: "Paulo das Couves",
+    nomeUsuario: "paulo",
+    senhaUsuario: "123456",
+  },
+  {
+    nomeCompleto: "Mary Help",
+    nomeUsuario: "mary",
+    senhaUsuario: "123456",
+  },
+  {
+    nomeCompleto: "Pedro Silva",
+    nomeUsuario: "pedro",
+    senhaUsuario: "123456",
+  },
+];
 
-//Declarando objetos
-const usuario1 = {
-  nomeUsuario: "denden",
-  senhaUsuario: "12345",
-  gravaDados: true,
-  token: tokenGerado,
-};
-
-const usuario2 = {
-  nomeUsuario: "gege",
-  senhaUsuario: "12345",
-  gravaDados: true,
-  token: tokenGerado,
-};
-
-let listaDeUsuarios = [];
-listaDeUsuarios.push(usuario1);
-listaDeUsuarios.push(usuario2);
+//GUARDAR A LISTA DE OBJETOS NO LOCAL STORAGE
+localStorage.setItem("listaUsers", JSON.stringify(listaDeUsuarios));
 
 addEventListener("click", (evt) => {
   const inputUser = document.querySelector("#idUser");
   const inputPass = document.querySelector("#idPass");
-  const iconEye = document.querySelector("#iconEye")
+  const iconEye = document.querySelector("#iconEye");
 
   if (evt.target.id == "btnSubmit") {
     try {
+      //RECUPERAR A LISTA DE USUÁRIOS DO LOCAL STORAGE
+
+      let listaDeUsuarios = JSON.parse(localStorage.getItem("listaUsers"));
+
       listaDeUsuarios.forEach((usuario) => {
         if (
           inputUser.value == usuario.nomeUsuario &&
@@ -44,6 +62,9 @@ addEventListener("click", (evt) => {
         msgError.setAttribute("style", "color:#00ff00");
         msgError.innerHTML =
           "<span><strong>Login bem-sucedido!</strong></span>";
+          setTimeout(function (){
+              window.location.href = "../pages/sucesso.html"
+          },2500);
         return;
       }
       msgError.setAttribute("style", "color:#ff0000");
@@ -55,8 +76,8 @@ addEventListener("click", (evt) => {
       inputPass.setAttribute("type", "text");
       iconEye.setAttribute("class", "fa fa-eye-slash");
     } else {
-        inputPass.setAttribute("type", "password");
-        iconEye.setAttribute("class", "fa fa-eye");
+      inputPass.setAttribute("type", "password");
+      iconEye.setAttribute("class", "fa fa-eye");
     }
   }
 });
